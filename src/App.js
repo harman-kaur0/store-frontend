137ee5f73c080550b574e5faf5a75fcf68fc4da9
OM Miles <miles@ommiles.com>
2021-06-22 16:57:08 -0700

    tuesday frontend code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Navbar from './Navbar';
+import { Route, Switch } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  state = {
+    login: "",
+  }
+
+  fetch()
+
+  handleLogin = () => <Login history={this.props.history} />
+  handleSignUp = () => <SignUp history={this.props.history} />
+  handleHome = () => <Home history={this.props.history}/>
+  handleUser = () => <User history={this.props.history} />
+
+  render() {
+    return (
+      <div>
+        <Navigation />
+        <Switch>
+          <Route path='/' exact component={this.handleLogin} />
+          <Route path='/signup' exact component={this.handleSignUp} />
+          <Route path='/home' exact component={this.handleHome} />
+          <Route path='/profile' exact component={this.handleUser} />
+        </Switch>
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ src/Login.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+
+export default class Login extends Component {
+  state = {
+    nameValue: "",
+    emailValue: "",
+    name: "",
+    email: "",
+  };
+
+  handleChange(e) {
+    const target = e.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleSubmit(e) {
+    alert("A name was submitted: " + this.state.value);
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={(e) => this.setState({ value: e.target.value })}>
+          <label>
+            Name:
+            <input
+              type="text"
+              name="name"
+              value={this.state.nameValue}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Email:
+            <input
+              type="text"
+              name="email"
+              value={this.state.emailValue}
+              onChange={this.handleChange}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  }
+}
