d3364b034f2c240467823b5f5bfd82d476655662
OM Miles <miles@ommiles.com>
2021-06-23 14:40:07 -0700

    pre-lunch code

--- src/Components/ProductList.js
+++ src/Components/ProductList.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import './ProductList.css'
 
 export default class ProductList extends Component {
   state = {
@@ -18,14 +19,30 @@ export default class ProductList extends Component {
     return (
       <div>
         <h1>Product List Here!</h1>
-        {this.state.products.map((p) => (
-          <div>
-            <li>{p.name}</li>
-            <li>{p.image}</li>
-            <li>$ {p.price}</li>
-            <li>{p.description}</li>
-          </div>
-        ))}
+        <div style={{display: "flex"}}>
+          {this.state.products.map((p) => (
+            <article>
+              <img src={p.image} alt="product thumbnail" style={{}}></img>
+              <div className="card-wrapper">
+                <div className="header-wrapper">
+                  <div className="h1-wrapper">
+                    <h1>
+                      {p.name}
+                    </h1>
+                  </div>
+                  <div className="h2-wrapper">
+                    <h2>
+                      $ {p.price}
+                    </h2>
+                  </div>
+                </div>
+                <p className="copy">
+                  {p.description}
+                </p>
+              </div>
+            </article>
+          ))}
+        </div>
       </div>
     );
   }
