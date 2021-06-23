64c16c53601e664b355f724573311d9a178e03b3
Harmandeep Kaur <harmandeepk628@gmail.com>
2021-06-23 16:28:08 -0700

    fetch comments

---
+++ src/Components/CommentForm.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import {withRouter} from 'react-router-dom'
+
+class Comment extends Component {
+    render (){
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default withRouter(Comment)
--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,15 +1,29 @@
 import { Component } from "react"
+import {withRouter} from 'react-router-dom'
 
 class Product extends Component{
-    itemId = this.props.location.pathname.split("/item/")[1]
+    itemId = parseInt(this.props.location.pathname.split("/item/")[1])
+
+    // handleClick = () => {
+    //     if (this.props.user){
+    //         this.props.history.push()
+    //     }
+    // }
+    
     render(){
+        let item = this.props.products.find(obj => obj.id === this.itemId)
+        console.log(this.props.comments)
         return (
             <div>
-    
+                <h1>{item.name}</h1>
+                <img src= {item.image}/>
+                <h4>$ {item.price}</h4>
+                <h6>Product Details: {item.description}</h6>
+                <button onClick={console.log("comment")}>Leave a Review</button>
             </div>
         )
     }
 }
 
 
-export default Product
+export default withRouter(Product)
